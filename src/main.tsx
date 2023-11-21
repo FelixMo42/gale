3dcd77a805fe1ce70d2dd9a636e1d0bd1531ec49
FelixMo42 <felix@tacocat.com>
2023-11-21 12:13:36 -0800

    add: reorderable todo list

--- src/main.tsx
+++ src/main.tsx
@@ -28,25 +28,97 @@ function updateTodoItemChecked(item: TodoItem, checked: boolean) {
     update(_state => item.checked = checked)
 }
 
+function reorderTodo(todo: TodoItem[]) {
+    update(state => state.todo = todo)
+}
+
+const drag: { selected?: HTMLElement, start?: number } = {}
+
+function pickup(e: MouseEvent, el: HTMLElement) {
+    el.classList.add("drag")
+
+    drag.start = e.y - el.offsetTop
+    drag.selected = el
+
+    offsetView(e)
+}
+
+function putdown() {
+    // clear out drag stuff
+    drag.selected.classList.remove("drag")
+    drag.selected === undefined
+
+    // reorder todo list
+    const list = drag.selected.parentElement.children
+    const order = []
+    for (let i = 0; i < list.length; i++) {
+        order.push(list.item(i).id)
+    }
+    reorderTodo(order.map((id) => state.todo.find((item) => item.id === id)))
+}
+
+document.onmousemove = (e) => {
+    if (drag.selected) {
+        // if were not clicking it any more, put it down
+        if (e.buttons !== 1) return
+
+        // get the postion of each item
+        const list = drag.selected.parentElement.children
+        const items = []
+        for (let i = 0; i < list.length; i++) {
+            const el = list.item(i)
+            const rect = el.getBoundingClientRect()
+            const y =
+                (drag.selected === el) ? e.clientY
+                : rect.top + rect.height / 2
+
+            items.push({ y, el })
+        }
+
+        // reorder the children
+        drag.selected.parentElement.replaceChildren(
+            ...items
+                .sort((a, b) => a.y - b.y)
+                .map(({ el }) => el)
+        )
+
+        // make so that the element follows the mouse
+        offsetView(e)
+    }
+}
+
+function offsetView(e) {
+    const y = e.clientY - drag.selected.offsetTop - drag.start
+    const view = drag.selected.firstChild as HTMLElement
+    view.style.top = `${y}px`
+}
+
+window.onmouseup = () => putdown()
+
 function TodoItemView({ item }: { item: TodoItem }) {
-    return <div>
-        <input
-            type="checkbox"
-            checked={item.checked}
-            onchange={(e) => updateTodoItemChecked(item, e.target.checked)}
-        />
-        <input
-            type="text"
-            class="item"
-            value={item.text}
-            onchange={(e) => updateTodoItemText(item, e.target.value)}
-        />
+    const el = <div id={item.id} onmousedown={(e) => pickup(e, el)}>
+        <span>
+            <input
+                type="checkbox"
+                checked={item.checked}
+                onchange={(e) => updateTodoItemChecked(item, e.target.checked)}
+            />
+            <input
+                type="text"
+                class="item"
+                value={item.text}
+                onchange={(e) => updateTodoItemText(item, e.target.value)}
+            />
+        </span>
     </div>
+
+    return el
 }
 
 function AddTodoItemButton() {
     return <input
         type="text"
+        id="add-todo"
         placeholder="+ add todo item"
         onchange={(e) => addTodoItem(e.target.value)}
     />
@@ -54,7 +126,9 @@ function AddTodoItemButton() {
 
 export default function main() {
     return <div class="todo">
-        {state.todo.map(item => <TodoItemView item={item} />)}
+        <div>
+            {state.todo.map(item => <TodoItemView item={item} />)}
+        </div>
         <AddTodoItemButton />
     </div>
 }
