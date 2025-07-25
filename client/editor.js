4ab4dc94a71ba52892ed51d2484ac63d4c2ae332
Felix Moses <felix@tacocat.com>
2025-07-25 18:53:08 +0200

    editor 2.0

--- client/editor.js
+++ client/editor.js
@@ -20,79 +20,105 @@ window.addEventListener("load", () => {
 function attach_editor(editor) {
     const path = editor.getAttribute("href")
 
-    // fetch the content of the linked note
     fetch(path, { cache: "no-cache" })
         .then(response => response.status == 200 ? response.text() : "")
         .then(text => editor.innerHTML = text
             .split("\n")
-            .map((line) => `<div>${line}</div>`)
+            .map((line, i) => `<div class=${i}>${line_to_html(line)}</div>`)
             .join("")
         )
-        .then(() => format(editor))
 
-    // reformat & save on edit
-    editor.addEventListener('input', () => {
-        format(editor)
+    editor.addEventListener('keydown', (event) => {
+        const edit = key_to_edit(editor, format_key(event))
 
-        const body = get_editor_text(editor)
-        fetch(path, { method: "POST", body })
+        if (edit) {
+            if (format_key(event) != "[cmd]z") add_undo_event(edit)
+            event.preventDefault()
+        }
+    })
+
+    editor.addEventListener('paste', function(event) {
+        const area = get_area(editor)
+        const text = event.clipboardData.getData('text/plain')
+        const edit = replace(editor, area, text)
+        add_undo_event(edit)
+        event.preventDefault()
     })
 }
 
-function get_editor_text(html) {
-    return [...html.childNodes].map(node => {
-        if (node.nodeType == node.TEXT_NODE) return node.textContent
-        if (node.innerText === "\n") return ""
-        return node.innerText
-    }).join("\n")
+function format_key(event) {
+    return [
+        event.metaKey ? "[cmd]" : "",
+        (event.metaKey && event.shiftKey) ? "[shift]" : "",
+        event.key,
+    ].join("")
 }
 
-function get_parent_div(element) {
-    if (element.tagName === "DIV") {
-        return element
+const undos = []
+const redos = []
+
+function add_undo_event(edit) {
+    const can_merge_new = (
+        undos.length > 0 &&
+        !edit.old &&
+        !undos.at(-1).old &&
+        edit.area[0] == undos.at(-1).area[0] + undos.at(-1).new.length
+    )
+
+    const can_merge_old = (
+        undos.length > 0 &&
+        !edit.new &&
+        !undos.at(-1).new &&
+        edit.area[1] == undos.at(-1).area[0]
+    )
+
+    if (can_merge_new) {
+        undos.at(-1).new += edit.new
+    } else if (can_merge_old) {
+        undos.at(-1).old = edit.old + undos.at(-1).old
+        undos.at(-1).area[0] = edit.area[0]
     } else {
-        return get_parent_div(element.parentElement)
+        undos.push(edit)
     }
 }
 
-function save_caret() {
-    const selection = window.getSelection()
-    if (selection.rangeCount === 0) return () => {}
+function key_to_edit(editor, key) {
+    const area = get_area(editor)
 
-    const range = selection.getRangeAt(0)
-    const parent = get_parent_div(range.startContainer)
-    const offset_range = document.createRange()
-    offset_range.setStart(parent, 0)
-    offset_range.setEnd(range.endContainer, range.endOffset)
-    const offset = offset_range.toString().length
-
-    return function restore_caret() {
-        const walker = document.createTreeWalker(
-            parent,
-            NodeFilter.SHOW_TEXT
-        )
-
-        let position = 0;
-        let node;
-        while (node = walker.nextNode()) {
-            if (position + node.textContent.length >= offset) {
-                const new_range = document.createRange()
-                new_range.setStart(node, offset - position)
-                new_range.collapse(true)
-
-                selection.removeAllRanges()
-                selection.addRange(new_range)
-
-                return
-            } else {
-                position += node.textContent.length
+    if (key === "Backspace") {
+        if (area[0] == area[1]) {
+            if (area[0] > 0) {
+                return replace(editor, [area[0] - 1, area[0]], "")
             }
+        } else {
+            return replace(editor, area, "")
+        }
+    } else if (key === "Enter") {
+        return replace(editor, area, "\n")
+    } else if (key.length === 1) {
+        return replace(editor, area, event.key)
+    } else if (key == "[cmd]z") {
+        if (undos.length) {
+            const edit = undo(editor, undos.pop())
+            redos.push(edit)
+            return edit
+        }
+    } else if (key == "[cmd][shift]z") {
+        if (redos.length) {
+            return undo(editor, redos.pop())
         }
     }
 }
 
+function undo(editor, edit) {
+    return replace(editor,
+        [edit.area[0], edit.area[0] + edit.new.length ?? 0],
+        edit.old ?? "",
+    ) 
+}
+
 function line_to_html(line) {
-    if (line.trim().length === 0) return "<br>"
+    if (line.length == 0) return "<br>"
 
     return match_replace(line, [
         [/\*[^\*]*\*/g, g => `<i>${g}</i>`],
@@ -100,6 +126,7 @@ function line_to_html(line) {
 
         [/\*\*[^\*]*\*\*/g, g => `<b>${g}</b>`],
         [/\_\_[^\_]*\_\_/g, g => `<b>${g}</b>`],
+        [/\=\=[^\=]*\=\=/g, g => `<span class="highlight">${g}</span>`],
 
         [/\~\~.*\~\~/g, g => `<span class="strike">${g}</span>`],
 
@@ -114,35 +141,6 @@ function line_to_html(line) {
     ])
 }
 
-function get_line_class(line) {
-    if (line.startsWith("# ")) return "h1"
-    if (line.startsWith("## ")) return "h2"
-    return ""
-}
-
-function format(editor) {
-    const restore_caret = save_caret(editor)
-
-    const lines = editor.querySelectorAll("div")
-    if (lines.length === 0) {
-        editor.innerHTML = line_to_html(editor.textContent)
-        editor.firstChild.className = get_line_class(node.textContent)
-    } else {
-        lines.forEach(node => {
-            node.innerHTML = line_to_html(node.textContent)
-            node.className = get_line_class(node.textContent)
-            node.style = ""
-        })
-    }
-    
-    restore_caret()
-
-    editor.querySelectorAll(".indent").forEach((element) => {
-        element.parentElement.style.paddingLeft = `${element.scrollWidth}px`
-        element.parentElement.style.textIndent = `-${element.scrollWidth}px`
-    })
-}
-
 function match_replace(line, patterns) {
     for (const pattern of patterns) {
         const matches = line.match(pattern[0])
@@ -154,6 +152,147 @@ function match_replace(line, patterns) {
     }
 
     return line
+}
+
+function get_document_fragment_text(frag) {
+    if (frag.childNodes?.[0]?.tagName === "DIV") {
+        return [...frag.childNodes]
+            .map((child) => child.textContent)
+            .join("\n")
+    }
+
+    return frag.textContent
+}
+
+function replace(editor, area, text) {
+    const edit = { area, new: text }
+
+    // delete old text in the range
+    if (area[0] != area[1]) {
+        const range = area_to_range(editor, area)
+
+        const a = get_parent_div(range.startContainer)
+        const b = get_parent_div(range.endContainer)
+
+        const frag = range.extractContents()
+        
+        edit.old = get_document_fragment_text(frag)
+
+        const accross_lines = a != b
+
+        if (accross_lines) {
+            a.appendChild(...b.childNodes)
+            b.remove()
+        }
+    }
+
+    const range = area_to_range(editor, [area[0], area[0]])
+
+    // insert the text, if any
+    if (text) {
+        const el = document.createTextNode(text)
+        range.insertNode(el)
+    }
+
+    const line_el = get_parent_div(range.startContainer)
+    
+    line_el.textContent
+        .split("\n")
+        .map(line => create_line_el(line))
+        .forEach(el => line_el.parentNode.insertBefore(el, line_el))
+    
+    line_el.remove()
+
+    // move caret to the right place
+    const caret = area[0] + text.length
+    const selection = this.getSelection()
+    selection.removeAllRanges()
+    selection.addRange(area_to_range(editor, [caret, caret]))
+
+    return edit
+}
+
+function area_to_range(editor, area) {
+    const range = document.createRange()
+    let start = true
+
+    walk(editor, (node, index) => {
+        // check start position
+        if (start && area[0] <= (index + node.textContent.length)) {
+            start = false
+            range.setStart(node, area[0] - index)
+        }
+
+        // check end position
+        if (!start && area[1] <= (index + node.textContent.length)) {
+            range.setEnd(node, area[1] - index)
+            return true
+        }
+    })
+
+    return range
+}
+
+function get_caret_range() {
+    try {
+        return this.getSelection().getRangeAt(0)
+    } catch {
+        return undefined
+    }
+}
+
+function get_area(editor) {
+    const range = get_caret_range()
+    if (!range) return undefined
+
+    const area = [0, 0]
+    let start = true
+
+    walk(editor, (node, index) => {
+        // check start position
+        if (start && node === range.startContainer) {
+            area[0] = index + range.startOffset
+            start = false
+        }
+
+        // check end position
+        if (!start && node === range.endContainer) {
+            area[1] = index + range.endOffset
+            return true
+        }
+    })
+
+    return area
+}
+
+function walk(editor, fn, index=-1) {
+    const walker = document.createTreeWalker(editor)
+    while (walker.nextNode()) {
+        if (walker.currentNode.nodeName === "#text") {
+            if (fn(walker.currentNode, index)) return
+            index += walker.currentNode.textContent.length
+        } else if (walker.currentNode.tagName === "DIV") {
+            index += 1
+
+            if (walker.currentNode.innerHTML === "<br>") {
+                if (fn(walker.currentNode, index)) return
+            }
+        }
+    }
+}
+
+function get_parent_div(element) {
+    if (element.tagName === "DIV") {
+        return element
+    } else {
+        return get_parent_div(element.parentElement)
+    }
+}
+
+function create_line_el(text) {
+    const el = document.createElement("div")
+    el.innerHTML = line_to_html(text)
+    return el
 }
 
 function format_link(name) {
