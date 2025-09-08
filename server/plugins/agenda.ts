4db8ccab3bca0d607eaadee75f39311f2c7ce471
Felix Moses <felix@tacocat.com>
2025-09-08 11:56:41 +0200

    no more gauth :(

--- server/plugins/agenda.ts
+++ server/plugins/agenda.ts
@@ -1,12 +1,7 @@
-import { readFile } from "node:fs/promises"
-
 import { _, PageResponse } from "../lib/html.ts"
-import { gfetch } from "../lib/gauth.ts"
-import { exists, range } from "../lib/utils.ts"
+import { range } from "../lib/utils.ts"
 import { Request } from "../lib/router.ts"
 import * as time from "../lib/time.ts"
-
-import { config } from "../../config.ts"
 
 export function agenda(req: Request) {
     if (req.url === "/agenda") {
@@ -74,71 +69,3 @@ _.agenda = (attrs, _children) => {
         ])
     ])
 }
-
-async function get_agenda(date=new Date()): Promise<string> {
-    const path = `${config.notes_dir}/.hidden/agenda/${time.format_date_file(date)}.md`
-
-    if (await exists(path)) {
-        return readFile(path, "utf-8")
-    }
-
-    const agenda = range((24 - 8) * 2).map(() => "")
-
-    for (const event of await get_todays_events(date)) {
-        agenda[get_agenda_slot(event.start!)] = event.name
-        if (get_agenda_slot(event.end!) > get_agenda_slot(event.start!) + 1) {
-            agenda[get_agenda_slot(event.end!) - 1] = "---"
-        }
-    }
-
-    return agenda.join("\n")
-}
-
-async function get_todays_events(date=new Date()): Promise<Event[]> {
-    const gevent = (await get_events(date).catch(() => []))
-        .filter(event => !!event.start)
-
-    return [
-        ...config.events,
-        ...gevent,
-    ].sort((a, b) => a.start!.getTime() - b.start!.getTime())
-}
-
-async function get_events(date=new Date()) {
-    const { time_min, time_max } = time.get_date_range(date)
-
-    const calendars = await gfetch<{
-        items: {
-            id: string
-        }[]
-    }>("/calendar/v3/users/me/calendarList")
-
-    const events = await Promise.all(calendars.items.map(calendar =>
-        gfetch<{
-            items: {
-                id: string,
-                colorId: string,
-                summary: string,
-                start: { dateTime: string },
-                end: { dateTime: string },
-            }[]
-        }>(`/calendar/v3/calendars/${calendar.id}/events`, {
-            "timeMin": time_min,
-            "timeMax": time_max,
-            "singleEvents": "true",
-            "orderBy": "startTime",
-            "maxResults": "20",
-        })
-    ))
-
-    return events.flatMap(event => event.items.map(item => ({
-        name: item.summary,
-        start: item.start.dateTime ? new Date(item.start.dateTime) : undefined,
-        end: item.end.dateTime ? new Date(item.end.dateTime) : undefined,
-    })))
-}
-
-function get_agenda_slot(time: Date) {
-    if (time.getHours() === 0) return (24 - 8) * 2
-    return (time.getHours() - 8) * 2 + Math.floor(time.getMinutes() / 30)
-} 
